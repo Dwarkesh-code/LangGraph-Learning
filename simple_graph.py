959138f1c978c8a0dec69dc1b6d8975fc981ceb0
Dwarkesh-code <dwarkeshcode@gmail.com>
2026-07-15 21:42:27 +0530

    add memory saver

--- simple_graph.py
+++ simple_graph.py
@@ -1,9 +1,9 @@
 from langchain_groq import ChatGroq
 from langgraph.graph import StateGraph, START, END, MessagesState
 from dotenv import load_dotenv
-from langgraph.prebuilt import ToolNode
+from langgraph.prebuilt import ToolNode, tools_condition
 from langchain.tools import tool
-
+from langgraph.checkpoint.memory import MemorySaver
 
 load_dotenv()
 
@@ -24,16 +24,21 @@ llm_with_tools= llm.bind_tools([multiply])
 def model(state:MessagesState):
     return {"messages": [llm_with_tools.invoke(state['messages'])]} 
     
-
+memory = MemorySaver()
+config = {"configurable": {"thread_id": "1"}}
 
 graph = StateGraph(MessagesState)
 graph.add_node("model", model)
 graph.add_node("tools", tool_nodes)
 graph.add_edge(START, "model")
-graph.add_edge("model", "tools")
-graph.add_edge("tools", END)
-final = graph.compile()
+graph.add_conditional_edges("model", tools_condition)
+graph.add_edge("tools", "model")
+final = graph.compile(checkpointer=memory)
 
-result = final.invoke({"messages": ["multiply 2 and 293 "]})
+result = final.invoke({"messages": ["multiply 2 and 293 "]}, config)
 
-print("output: \n",result['messages'])
+print("output: \n",result['messages'][-1].content,"\n\n\n")
+
+result = final.invoke({"messages": ["multiply 7 and 34 "]}, config)
+
+print("output: \n",result['messages'][-1].content)
